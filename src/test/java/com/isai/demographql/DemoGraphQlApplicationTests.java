65bdf4dcecfd5bea739275ef5aa4eb9479181883
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-14 02:04:01 -0500

    init

---
+++ src/test/java/com/isai/demographql/DemoGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
