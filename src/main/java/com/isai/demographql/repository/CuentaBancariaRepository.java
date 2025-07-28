0e43e87cfc9fe68eb37f033b8c5c48d5f8dec700
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-28 00:01:27 -0500

    repositorio para cuenta bancaria

--- src/main/java/com/isai/demographql/repository/CuentaBancariaRepository.java
+++ src/main/java/com/isai/demographql/repository/CuentaBancariaRepository.java
@@ -3,6 +3,7 @@ package com.isai.demographql.repository;
 
 import com.isai.demographql.models.CuentaBancaria;
 import com.isai.demographql.models.TipoCuenta;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -10,7 +11,8 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import java.util.List;
 
 @RepositoryRestResource
-public interface CuentaBancariaRepository {
+public interface CuentaBancariaRepository
+        extends JpaRepository<CuentaBancaria, String> {
 
     @RestResource(path = "/byType")
     List<CuentaBancaria> findByTipoCuenta(@Param("t") TipoCuenta tipoCuenta);
