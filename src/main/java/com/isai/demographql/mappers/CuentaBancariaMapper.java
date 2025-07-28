d089588b5d9691ab4944f9251be9efa0e0ab2b05
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-27 23:47:50 -0500

    renombre CuentaBancariaResponseDTO

--- src/main/java/com/isai/demographql/mappers/CuentaBancariaMapper.java
+++ src/main/java/com/isai/demographql/mappers/CuentaBancariaMapper.java
@@ -1,15 +1,15 @@
 package com.isai.demographql.mappers;
 
-import com.isai.demographql.dtos.CuentaBancariaDTO;
+import com.isai.demographql.dtos.CuentaBancariaResponseDTO;
 import com.isai.demographql.models.CuentaBancaria;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CuentaBancariaMapper {
-    public CuentaBancariaDTO fromCuentaBancaria(CuentaBancaria cuentaBancaria) {
-        CuentaBancariaDTO cuentaBancariaDTO = new CuentaBancariaDTO();
-        BeanUtils.copyProperties(cuentaBancaria, cuentaBancariaDTO);
-        return cuentaBancariaDTO;
+    public CuentaBancariaResponseDTO fromCuentaBancaria(CuentaBancaria cuentaBancaria) {
+        CuentaBancariaResponseDTO cuentaBancariaResponseDTO = new CuentaBancariaResponseDTO();
+        BeanUtils.copyProperties(cuentaBancaria, cuentaBancariaResponseDTO);
+        return cuentaBancariaResponseDTO;
     }
 }
